b398c160f111d7cadda424082f9bd51127dc0dea
1505-prinsu <patelkinjal2308@gmail.com>
2025-10-28 16:52:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -460,7 +460,6 @@ async def txt_handler(bot: Client, m: Message):
                     async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
                         text = await resp.text()
                         url = re.search(r"(https://.*?playlist.m3u8.*?)\"", text).group(1)
-
       #      if "/playlist.m3u8" in url :
       #          if "classplusapp.com/drm/" in url:
      #               url = "https://dragoapi.vercel.app/classplus?link=" + url
@@ -478,9 +477,7 @@ async def txt_handler(bot: Client, m: Message):
    #                         await m.reply_text(f"got keys form api : \n`{key}`")
 #                        else:
      #                       print(f"Failed to get key, status code: {resp.status}")
-    #                        await m.reply_text(f"Failed to get key from API, status code: {resp.status}")
-
-            if "acecwply" in url:
+    #                        await m.reply_text(f"Failed to get key from API, status code: {resp.status}")            if "acecwply" in url:
                 cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mkv --no-warning "{url}"'
                 
             elif "tencdn.classplusapp" in url or "media-cdn-alisg.classplusapp.com" in url or "videos.classplusapp" in url or "media-cdn.classplusapp" in url:
